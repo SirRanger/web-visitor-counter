958ab5003d84df0c33267658f4a13ee7f35c8788
Dominik <d.business@outlook.de>
2023-10-29 16:33:54 +0100

    update

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ def get_visitor_count():
 def update_visitor_count(count):
     """Function to update visitor count in file"""
     with open('daten.txt', 'a', encoding="utf-8") as file:
-        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H')
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
         file.write(f"{timestamp}: {count}\n")
 
 
@@ -30,15 +30,19 @@ def count_visitors_last_hour():
     with open('daten.txt', 'r', encoding="utf-8") as file:
         lines = file.readlines()
     current_time = datetime.datetime.now()
-    one_hour_ago = current_time - datetime.timedelta(hours=1)   
+    one_hour_ago = current_time - datetime.timedelta(hours=1)
     count = 0
     for line in lines:
         parts = line.split(':')
         if len(parts) >= 2:
-            timestamp = datetime.datetime.strptime(parts[0], '%Y-%m-%d %H')
+            date_time_part = parts[0].replace('-', ':')  # Replace hyphens with colons
+            timestamp = datetime.datetime.strptime(date_time_part, '%Y:%m:%d %H:%M:%S')
             if one_hour_ago <= timestamp <= current_time:
-                count += int(parts[-1])   
+                count += int(parts[-1])
     return count
+
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -70,4 +74,4 @@ def admin():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5050)
